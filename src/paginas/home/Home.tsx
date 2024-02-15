f9e2a09c3fdb16dcef47986be02721a45c05fbbc
Lucky2109 <lukao2109@gmail.com>
2024-02-15 00:12:51 -0300

    atividade react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Manipulando o mundo</h1>
+           <img className='img'src='https://i.ytimg.com/vi/vQpi9EQmcX0/maxresdefault.jpg' alt='bluezão'></img>
+        </>
+    );
+}
+
+export default Home;
